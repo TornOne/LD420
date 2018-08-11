d629f9901f239c51b5e471c53b989990985e594c
Joosep Jääger <joosep.jaager@gmail.com>
2018-08-11 16:05:10 +0300

    Added basic AI

---
+++ Assets/Scripts/CustomerAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class CustomerAI : MonoBehaviour {
+
+	public Transform seatsNode;
+
+	private Transform seat;
+	private UnityEngine.AI.NavMeshAgent navAgent;
+
+	void PickRandomSeat(){
+		seat = seatsNode.GetChild((int) Random.Range(0, seatsNode.childCount));
+	}
+
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		PickRandomSeat();
+		navAgent.SetDestination(seat.position);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
