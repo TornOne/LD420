b182cfa4ce8bec217e549055cd263f18eef7b9c4
Joosep Jääger <joosep.jaager@gmail.com>
2018-08-13 02:48:21 +0300

    Fixed corpse cleaning

---
+++ Assets/Scripts/CorpseCleaner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class CorpseCleaner : MonoBehaviour {
+
+	public float cleaningInterval = 5f;
+	private BoxCollider collider;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<BoxCollider>();
+		StartCoroutine(CorpseCleaningCoroutine());
+	}
+
+	IEnumerator CorpseCleaningCoroutine(){
+		yield return new WaitForSeconds(cleaningInterval);
+		CustomerAI[] ais = FindObjectsOfType<CustomerAI>();
+		foreach(CustomerAI ai in ais){
+			if(collider.bounds.Contains(ai.transform.position)){
+				if(ai.state == CustomerAI.State.dead || ai.state == CustomerAI.State.struggling){
+					Destroy(ai.gameObject);
+				}
+			}
+		}
+	}
+}
