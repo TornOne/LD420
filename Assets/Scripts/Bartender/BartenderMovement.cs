a914e0dbef1bc12400869b34555e12f7342a21a9
TornOne <ott.adermann@gmail.com>
2018-08-12 16:27:52 +0300

    movement fix

--- Assets/Scripts/Bartender/BartenderMovement.cs
+++ Assets/Scripts/Bartender/BartenderMovement.cs
@@ -10,6 +10,8 @@ public class BartenderMovement : MonoBehaviour {
 		Vector3 moveDirection = transform.TransformDirection(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 		if (moveDirection.magnitude >= 0.5f) {
 			controller.SimpleMove(moveDirection.normalized * speed);
+		} else {
+			controller.SimpleMove(new Vector3());
 		}
 		transform.Rotate(0, Input.GetAxis("Mouse X") * rotationSpeed, 0, Space.World);
 	}
--- Assets/Scripts/Bartender/DrinkFiller.cs
+++ Assets/Scripts/Bartender/DrinkFiller.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
 
 public class DrinkFiller : MonoBehaviour {
+	public GameObject UI;
 	public Tray tray;
 	public string drinkType;
+	public float fillTime = 1;
+
+	void OnMouseEnter() {
+		UI.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		UI.SetActive(false);
+	}
+
+	private void OnMouseUp() {
+		
+	}
 
 	void OnMouseDown() {
 		if ((tray.transform.position - transform.position).magnitude <= 1.5f && tray.isCarried) {
